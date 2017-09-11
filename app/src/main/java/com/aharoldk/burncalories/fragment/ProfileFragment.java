45e4ebb9dd6fbb92a9f1a5af94f4efc6d5e18a59
Alwandy Harold Kalangi <aharoldk@gmail.com>
2017-09-11 18:40:01 +0800

    Profile Maybe Done

--- app/src/main/java/com/aharoldk/burncalories/fragment/ProfileFragment.java
+++ app/src/main/java/com/aharoldk/burncalories/fragment/ProfileFragment.java
@@ -2,7 +2,10 @@ package com.aharoldk.burncalories.fragment;
 
 
 import android.database.Cursor;
+import android.icu.text.DecimalFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,24 +20,44 @@ import butterknife.ButterKnife;
 
 public class ProfileFragment extends Fragment {
     @BindView(R.id.tvName) TextView tvName;
+    @BindView(R.id.tvAvgSteps) TextView tvAvgSteps;
+    @BindView(R.id.tvAvgDistance) TextView tvAvgDistance;
+    @BindView(R.id.tvAvgCal) TextView tvAvgCal;
 
-    private DatabaseHelper databaseHelper;
+    double totalDistance;
+    double totalSteps;
+    double totalCalories;
 
     public ProfileFragment() {
-        // Required empty public constructor
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         ButterKnife.bind(this, view);
-        databaseHelper = new DatabaseHelper(getActivity());
+        DatabaseHelper databaseHelper = new DatabaseHelper(getActivity());
 
         Cursor cursor = databaseHelper.selectUser();
         cursor.moveToFirst();
         tvName.setText(String.valueOf(cursor.getString(1)));
+
+        Cursor cursorActivity = databaseHelper.selectActivity();
+
+        while(cursorActivity.moveToNext()){
+            totalDistance += cursorActivity.getDouble(1);
+            totalCalories += cursorActivity.getDouble(2);
+            totalSteps += cursorActivity.getDouble(3);
+
+        }
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+        tvAvgSteps.setText(String.valueOf(decimalFormat.format(totalSteps / cursorActivity.getCount())));
+        tvAvgDistance.setText(String.valueOf(decimalFormat.format(totalDistance / cursorActivity.getCount())));
+        tvAvgCal.setText(String.valueOf(decimalFormat.format(totalCalories / cursorActivity.getCount())));
 
         return view;
     }
